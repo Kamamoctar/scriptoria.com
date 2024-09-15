fdfd3490ece96a3cebc27eb5c7cb095bd0cb8800
Moctar K. NAMADOU <kamamoctar@gmail.com>
2024-09-15 08:22:57 +0000

    VOILA

---
+++ app.js
@@ -0,0 +1,7 @@
+new Typewriter('#typewriter', {
+    strings: ['Vos documents', 'Notre Metier', 'Votre partenaire pour la gestion d\'information'],
+    autoStart: true,
+    loop: true,
+    cursor: '|*'
+});
+console.log('Marche Aussi');
